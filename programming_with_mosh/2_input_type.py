4bdf3bbf829edf981ae30b7d8add9937d583572d
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-04-23 22:26:11 +0600

    Delete 2_input_type.py

--- programming_with_mosh/2_input_type.py
+++
@@ -1,8 +0,0 @@
-name = input("What is Your Name? ")
-print("Hi " +name)
-
-#Qst : Ask Name and fvrt color and print out
-
-name2 = input("What is ur name? ")
-color = input("Favourite color? ")
-print(name2 + " likes " + color)
