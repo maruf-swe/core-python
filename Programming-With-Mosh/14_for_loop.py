62bcf1304f8ee1d1bb510c2a5de4e1d7d0aa2ae5
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-11 05:41:36 +0600

    for loop

---
+++ Programming-With-Mosh/14_for_loop.py
@@ -0,0 +1,23 @@
+for item in 'python':
+    print(item)
+
+for item in ['Maruf', 'Isha', 'Pranti']:
+    print(item)
+
+for item in range(int(input("insert: "))):
+    print(item)
+
+for i in range(10):
+    print(i)
+
+for i in range(5, 10):
+    print(i)
+
+for i in range(5, 10, 2):
+    print(i)
+
+prices = [10,20,30]
+total = 0
+for price in prices:
+    total +=price
+print(f"total is :{total}")
