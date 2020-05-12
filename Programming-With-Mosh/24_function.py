4960d07ad61749116c9135366c813ee7f603e0df
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-12 07:25:07 +0600

    Add files via upload

---
+++ Programming-With-Mosh/24_function.py
@@ -0,0 +1,18 @@
+def great_user():
+    print("Hi there")
+    print("Welcome")
+
+
+great_user()
+
+def great_users(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome")
+
+
+great_users("Maruf", "Ahmed")
+
+def square(number):
+    return  number * number
+
+print(square(3))
---
+++ Programming-With-Mosh/25_keyword_argument.py
@@ -0,0 +1,6 @@
+def great_users(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome")
+
+
+great_users(last_name="Maruf", first_name="Ahmed")
