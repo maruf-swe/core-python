46dffec976e310b607a7b0917da6a2bb91311e9d
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-16 01:18:08 +0600

    first file

---
+++ Automate-the-boring-stuff/python_basic1.py
@@ -0,0 +1,43 @@
+price = 10 #int
+rating = 4.9 #float
+name = "Marufur Rahman" #string
+is_published = True #bolean variable
+print(price)
+
+print("Qst:we cheak a patient name John smith 20 years old and new patient")
+
+full_name = "John Smith"
+age = 20
+is_new = True
+
+name2 = input("What is ur name? ")
+color = input("Favourite color? ")
+print(name2 + " likes " + color)
+
+print("Operator")
+x= 5
+y =4
+print(x+y)
+c = x - y
+print(c)
+c= x * y
+print(c)
+c = x / y
+print(c)
+c = x // y
+print(c)
+
+print("Augment Operator")
+x=10
+x += 2
+print(x)
+x -= 5
+print(x)
+
+print("Operator Precedence")
+x = 10 + 3 * 2 ** 2
+print(x)
+x = (10 + 3) * 2 ** 2
+print(x)
+x = (2 + 3) * 10 -3
+print(x)
