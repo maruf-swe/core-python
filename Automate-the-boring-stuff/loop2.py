6f332f9a50a336b15f63db7a7dd7a1ba0cc63322
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-05-16 01:51:30 +0600

    upload loop files

---
+++ Automate-the-boring-stuff/loop2.py
@@ -0,0 +1,25 @@
+print("For loop")
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+for i in range(12, 16):
+    print(i)
+
+for i in range(0, 10, 2):
+    print(i)
+
+value = int(input("please insert value: "))
+for i in range(value):
+    print(i)
+
+for i in range(int(input())):
+    print(i)
+
+m=[1,2,3,4]
+for i in m:
+    print(i)
+else:
+    print("loop executed")
