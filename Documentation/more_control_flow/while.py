209dfdaa2342c864ca476ba8a958359dea517098
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-09-27 11:43:40 +0600

    Create while.py

---
+++ Documentation/more_control_flow/while.py
@@ -0,0 +1,33 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+# break statement
+i =0
+while i < 6:
+    if i == 2:
+        break
+    print(i)
+    i +=1
+
+# continue statement
+i =0
+while i < 6:
+    if i == 2:
+        continue
+    print(i)
+    i +=1
+
+# else statement
+print("else statement")
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+#infinite loop
+while True:
+    print("I love u")
