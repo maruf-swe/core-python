5efd7d97b276c2840663de130b4b61c24c32a4b2
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-09-27 10:52:17 +0600

    Create for_while.py

---
+++ Documentation/more_control_flow/for_while.py
@@ -0,0 +1,7 @@
+words = ['banana', 'dragon', 'apple', 'pineaple']
+for w in words:
+    print(w, len(w))
+
+words = {'b': 'banana', 'd': 'dragon', 'a': 'apple', 'p': 'pineaple'}
+for key, w in words.items():
+    print(key, w)
