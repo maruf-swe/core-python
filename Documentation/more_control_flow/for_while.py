c3a603dafde8ca044d1ed03ad0f016d15a556b3b
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2020-09-27 10:52:55 +0600

    Update for_while.py

--- Documentation/more_control_flow/for_while.py
+++ Documentation/more_control_flow/for_while.py
@@ -5,3 +5,18 @@ for w in words:
 words = {'b': 'banana', 'd': 'dragon', 'a': 'apple', 'p': 'pineaple'}
 for key, w in words.items():
     print(key, w)
+
+# r range function
+for i in range(10):
+    print(i)
+
+for i in range(5, 20):
+    print(i)
+for i in range(5, 20, 3):
+    print(i)
+
+    # range() and len()
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i,a[i])
